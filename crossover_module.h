1b979b854941886aec859e97a7d6d492fea73a20
Arshia Akbari Alagha <arshia.alagha@gmail.com>
2024-02-03 22:21:22 +0330

    added module for Crossover

---
+++ crossover_module.h
@@ -0,0 +1,32 @@
+#include <systemc.h>
+#include "macros.h"
+
+SC_MODULE(CrossoverModule) {
+    sc_in<double> parent1_in[SOLUTION_SIZE];
+    sc_in<double> parent2_in[SOLUTION_SIZE];
+    sc_in<int> crossover_point_in; 
+    sc_out<double> child1_out[SOLUTION_SIZE];
+    sc_out<double> child2_out[SOLUTION_SIZE];
+
+
+    void perform_crossover() {
+
+        for (int i = 0; i < SOLUTION_SIZE; i++) {
+            if (i < crossover_point_in.read()) {
+                child1_out[i]->write(parent1_in[i]);
+                child2_out[i]->write(parent2_in[i]);
+            }
+            else {
+                child1_out[i]->write(parent2_in[i]);
+                child2_out[i]->write(parent1_in[i]);;
+            }
+        }
+    }
+
+    SC_CTOR(CrossoverModule) {
+        SC_METHOD(perform_crossover);
+        for (int i = 0; i < SOLUTION_SIZE; i++) {
+            sensitive << parent2_in[i] << parent1_in[i] << crossover_point_in;
+        }
+    }
+};
